16518447c0b2184fc78b63cfb085946bd9a6cf28
Alessandro9827 <ale.moramarco27@gmail.com>
2024-01-16 15:58:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+//Dobbiamo creare una web-app che permetta di leggere una lista di dischi presente nel nostro server.
+//Html, CSS, VueJS (importato tramite CDN), axios, PHP
+//Prima assicuratevi che la vostra pagina index.php (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre “API”).
+//Solo a questo punto sarà utile passare alla lettura della lista da un file JSON che potete creare liberamente con almeno 6 album musicali.
+//Bonus: Al click su un disco, recuperare e mostrare i dati del disco selezionato.
+
+?>
